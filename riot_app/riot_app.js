2ed1096bdcbdbe8d18e096337fb46777fc33d529
ptgrimes <ptgrimes235@gmail.com>
2014-11-10 15:29:16 -0500

    Add Dragon Head

--- riot_app/riot_app.js
+++ riot_app/riot_app.js
@@ -1,19 +1,26 @@
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to riot_app.";
-  };
-
-  Template.hello.events({
-    'click input': function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+  Meteor.startup(function () {
+    if (typeof console !== 'undefined')
+    {
+      $.getJSON("https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion?champData=spells&api_key=c8a49068-20bf-40d1-8834-891b15913db3", function (data) { 
+        window.champData = data;
+        console.log(data)
+      })
+      $.getJSON("https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion?champData=image&api_key=c8a49068-20bf-40d1-8834-891b15913db3", function (data) { 
+        window.imageData = data;
+        console.log(data)
+      })
     }
   });
-}
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  Template.calculate.greeting = function () {
+    return "Welcome to the League of Efficiency.";
+  };
+
+  Template.calculate.events({
+    'click input': function () {
+      console.log(window.imageData[1]);
+      console.log(window.champData);
+    }
   });
 }
